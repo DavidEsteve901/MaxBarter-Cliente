a91a42572890efb19b2cff266139d379586bf0bd
David Esteve <desteve@vidal-ac.es>
2022-04-08 14:12:08 +0200

    DEJO SOLO CLIENTE

--- Servidor/app/controllers/auth.controller.js
+++
@@ -1,9 +0,0 @@
-import User from '../models/User'
-
-export const signUp = async (req, res) => {
-    res.json('signup')
-}
-
-export const signIn = async (req, res) => {
-    res.json('signIn')
-}
--- Servidor/app/routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-import * as authCtrl from '../controllers/auth.controller'
-
-export default router;
